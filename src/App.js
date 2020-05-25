b0c934b6aa42815b5e40b8e775de2f6d819f94e0
limzidi <limzidi@hotmail.com>
2020-05-25 15:57:24 +0800

    connected netlify

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>what</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          what
         </a>
       </header>
     </div>
